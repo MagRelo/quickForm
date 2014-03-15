ac1e05ad0f4b42a8f2a09c5bab714a43f5100b23
MagRelo <Magrelo404@gmail.com>
2014-03-14 18:23:39 -0600

    final minor css tweaks

--- app/scripts/services/formdata.js
+++ app/scripts/services/formdata.js
@@ -4,16 +4,117 @@ angular.module('quickFormApp')
   .service('formData', function formData() {
     // AngularJS will instantiate a singleton by calling "new" on this function
 
-    //default data
-    this.name = "Community Event";
+    this.name = "Contact",
     this.fields = [
-      {"display_name":"Event Name","display_priority":"10","display_common":true,"input_type":"text","name":"","id":"","value":"","placeholder":"","required":true,"pattern":"","attributes":[{"type":"number","name":"maxlength"}]},
-      {"display_name":"Location","display_priority":"10","display_common":true,"input_type":"text","name":"","id":"","value":"","placeholder":"","required":true,"pattern":"","attributes":[{"type":"number","name":"maxlength"}]},
-      {"display_name":"Event Date","display_priority":"30","display_common":true,"input_type":"date","name":"","id":"","value":"","placeholder":"","required":true,"pattern":"","attributes":[{"type":"number","name":"min"},{"type":"number","name":"max"},{"type":"number","name":"step"}]},
-      {"display_name":"Organizer","display_priority":"10","display_common":true,"input_type":"text","name":"","id":"","value":"","placeholder":"","required":false,"pattern":"","attributes":[{"type":"number","name":"maxlength"}]},
-      {"display_name":"Organizer Email","display_priority":"20","display_common":true,"input_type":"email","name":"","id":"","value":"","placeholder":"","required":false,"pattern":"","attributes":[{"type":"number","name":"maxlength"},{"type":"boolean","name":"multiple"}]},
-      {"display_name":"Expected turnout","display_priority":"55","display_common":"true","input_type":"number","name":"","id":"","value":"","placeholder":"","required":false,"pattern":"","attributes":[{"type":"number","name":"min"},{"type":"number","name":"max"},{"type":"number","name":"step"}]}
-    ];
+    {
+      "display_name": "Name",
+      "display_priority": "10",
+      "display_common": true,
+      "input_type": "text",
+      "name": "",
+      "id": "",
+      "value": "",
+      "placeholder": {
+        "text": ""
+      },
+      "required": true,
+      "pattern": {
+        "text": "",
+        "regex": ""
+      },
+      "attributes": [
+        {
+          "type": "number",
+          "name": "maxlength"
+        }
+      ],
+      "$$hashKey": "05B"
+    },
+    {
+      "display_name": "Email",
+      "display_priority": "20",
+      "display_common": true,
+      "input_type": "email",
+      "name": "",
+      "id": "",
+      "value": "",
+      "placeholder": {
+        "text": ""
+      },
+      "required": false,
+      "pattern": {
+        "text": "",
+        "regex": ""
+      },
+      "attributes": [
+        {
+          "type": "number",
+          "name": "maxlength"
+        },
+        {
+          "type": "boolean",
+          "name": "multiple"
+        }
+      ],
+      "$$hashKey": "05N"
+    },
+    {
+      "display_name": "Telephone",
+      "display_priority": "40",
+      "display_common": false,
+      "input_type": "tel",
+      "name": "",
+      "id": "",
+      "value": "",
+      "placeholder": {
+        "text": ""
+      },
+      "required": false,
+      "pattern": {
+        "text": "",
+        "regex": ""
+      },
+      "attributes": [
+        {
+          "type": "number",
+          "name": "maxlength"
+        }
+      ],
+      "$$hashKey": "05T"
+    },
+    {
+      "display_name": "Contact Type",
+      "display_priority": "40",
+      "display_common": false,
+      "input_type": "dropdown",
+      "name": "",
+      "id": "",
+      "value": "",
+      "placeholder": {
+        "text": ""
+      },
+      "required": true,
+      "options": [
+        {
+          "name": "Customer",
+          "value": "option 1",
+          "$$hashKey": "063"
+        },
+        {
+          "name": "Supplier",
+          "value": "option 2",
+          "$$hashKey": "064"
+        },
+        {
+          "name": "Employee",
+          "value": "option 3",
+          "$$hashKey": "065"
+        }
+      ],
+      "$$hashKey": "05Z"
+    }
+  ];
+
 
     this.clearForm = function(){
       this.name = '';
